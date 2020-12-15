b1d4f3d1e353343980c9d0ff4f491d9dfcc37bcb
余振华 <bihtyu@163.com>
2020-12-15 14:13:25 +0800

    update 006

--- submit/006_z_transform.js
+++ submit/006_z_transform.js
@@ -12,39 +12,40 @@ var convert = function(s, numRows) {
     numArr[i] = []
   }
   let rows = 0
-  let isAdd = true
+  let isRise = true
   for (let i = 0; i < sLen; i++) {
-    console.log(rows)
-    if (rows < numRows && isAdd) {
+    if (rows < numRows && isRise) {
       numArr[rows].push(sArr[i])
-      if (rows < numRows - 1) {
-        rows += 1
-      } else {
-        isAdd = false
-        rows = numRows - 2
-      }
+      rows++
     } else {
-      isAdd = false
+      if (isRise) {
+        rows = numRows <= 2 ? 0 : (numRows - 2)
+      }
       numArr[rows].push(sArr[i])
-      rows = rows - 1 < 0 ? 0 : rows - 1
-      if (rows === 0) {
+      if (numRows === 1) {
+        rows = 0
+      } else if (numRows === 2) {
         rows = 1
-        isAdd = true
+      } else {
+        isRise = false
+        rows--
+        if (rows === 0) {
+          isRise = true
+        }
       }
     }
   }
+  // 0 1 2 1 0 1 2 1 0
   let zStr = ''
-  console.log(numArr)
   numArr.forEach(subArr => {
+    console.log(subArr)
     zStr += subArr.join('')
   })
   return zStr
 }
 
 // console.log(convert('PAYPALISHIRING', 3))
+// console.log(convert('ABCDEFGH', 1))
 // console.log(convert('ABCDEFGH', 2))
-console.log(convert('ABCDEFGH', 3))
-
-// A   E
-// B D F H
-// C   G
+console.log(convert('LEETCODEISHIRING', 3))
+// console.log(convert('LEETCODEISHIRING', 4))
